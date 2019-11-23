1e5bae018cc9ee726992f21d5873064d6561fd2f
donnaken15 <myminecraftfriend15@yahoo.com>
2019-11-23 17:07:06 -0500

    optimizations

--- Webdriver Torso/Program.cs
+++ Webdriver Torso/Program.cs
@@ -10,7 +10,7 @@ namespace Webdriver_Torso
 
         static Size size = new Size(640, 360);
 
-        static Point textpos = new Point(8,332);
+        static Point textpos = new Point(8, 332);
 
         static Random rand = new Random();
 
@@ -23,39 +23,41 @@ namespace Webdriver_Torso
             get
             {
                 return new Rectangle(
-                    rand.Next(0, 640), rand.Next(0, 360), rand.Next(0, 640), rand.Next(0, 360));
+                    rand.Next(0, 540), rand.Next(0, 240), rand.Next(0, 540), rand.Next(0, 240));
             }
         }
 
         static Font font = new Font("Courier New", 8, FontStyle.Bold);
 
-        static void Main(string[] args)
+        [MTAThread]
+        static void Main()
         {
             prog = new Form();
+            prog.ShowIcon = false;
+            prog.MaximizeBox = false;
+            prog.MinimizeBox = false;
+            prog.Text = "Webdriver Torso Simulation";
+            prog.FormBorderStyle = FormBorderStyle.FixedSingle;
             prog.ClientSize = size;
+            prog.StartPosition = FormStartPosition.CenterScreen;
             prog.BackColor = Color.White;
             prog.Controls.Add(display);
             display.Dock = DockStyle.Fill;
             prog.Show();
-            while(true) {
-                display.Image = new Bitmap(size.Width, size.Height);
+            while (prog.Visible)
+            {
+                display.Image = new Bitmap(640, 360);
                 using (Graphics g = display.CreateGraphics())
                 {
                     g.FillRectangle(Brushes.Blue, newbox);
                     g.FillRectangle(Brushes.Red, newbox);
                     g.DrawString("aqua.flv - Slide " + frame.ToString("0000"), font, Brushes.Black, textpos);
-                    g.Dispose();
                 }
-                GC.Collect();
-                Console.Beep(rand.Next(400,2000), 1000);
-                prog.Refresh();
-                prog.Update();
+                Console.Beep(rand.Next(400, 2000), 1000);
                 Application.DoEvents();
                 frame++;
                 if (frame >= 10000)
                     frame = 0;
-                if (prog.IsDisposed || prog.Visible == false)
-                    break;
             }
         }
     }
